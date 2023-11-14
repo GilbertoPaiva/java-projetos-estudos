d63e4f9c13b03d65bdbc5e2fb26cc42267842f1d
GilbertoPaiva <89491606+GilbertoPaiva@users.noreply.github.com>
2023-11-14 13:24:33 +0000

    Primeiro Metodo

---
+++ Anatomia-Das-Classes-3/src/AnatomiaDasClasses3.java
@@ -0,0 +1,13 @@
+public class AnatomiaDasClasses3{
+public static void main(String[] args) {
+    
+String meuNome = "Gilberto";
+
+int anoFabricacao = 2022;
+
+boolean verdadeira = true;
+
+anoFabricacao = 2018;
+
+}
+}
