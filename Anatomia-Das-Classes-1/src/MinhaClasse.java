f73866d49da5e022a907d98d842f6119b1d86211
GilbertoPaiva <89491606+GilbertoPaiva@users.noreply.github.com>
2023-11-14 14:07:12 +0000

    Println

--- Anatomia-Das-Classes-1/src/MinhaClasse.java
+++ Anatomia-Das-Classes-1/src/MinhaClasse.java
@@ -1,7 +1,8 @@
 public class MinhaClasse {
 
     public static void main(String[] args) {
-        System.out.println("Que comecem os jogos, e sorte esteja ao seu favor.");
+        System.out.println("Que comecem os jogos, e sorte esteja ao seu favor");
+
     }
     
 }
