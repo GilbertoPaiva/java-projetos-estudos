5af65907c0a4a9d2e15d19788c95c53037db3fd7
GilbertoPaiva <89491606+GilbertoPaiva@users.noreply.github.com>
2023-11-17 23:28:50 +0000

    Controle de Fuxo sem iteração

--- edu/gilberto/primeirasemana/ContaBanco/src/ContaTerminal.java
+++ edu/gilberto/primeirasemana/ContaBanco/src/ContaTerminal.java
@@ -17,7 +17,8 @@ public static void main(String[] args) {
     System.out.println("Por favor digite o saldo da conta:");
     double saldo = scan.nextDouble();
 
-    System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é "
+    System.out.println("Olá " + nomeCliente + ", obrigado por criar um
+    a conta em nosso banco, sua agência é "
                 + agencia + ", conta " + numeroConta + " e seu saldo " + saldo + " já está disponível para saque.");
 
         // Fechando o scanner
