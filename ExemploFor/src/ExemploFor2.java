2a66d9b63ff5cd89b81a69f37e715b8a90d57a3c
GilbertoPaiva <89491606+GilbertoPaiva@users.noreply.github.com>
2023-11-21 15:37:05 +0000

    #

---
+++ ExemploFor/src/ExemploFor2.java
@@ -0,0 +1,10 @@
+public class ExemploFor2 {
+    public static void main(String[] args) {
+        String alunos[] = {"FELIPE", "JONAS", "JULIA","MARCOS"};
+
+        for (int x = 0; x < alunos.length; x++) {
+            System.out.println("O aluno no índice x=" + (x+1) + " é " + alunos[x]);
+        }
+    }
+    
+}
