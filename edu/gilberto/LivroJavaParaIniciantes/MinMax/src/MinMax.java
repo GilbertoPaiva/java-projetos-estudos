f6b4441da07e4b211b1b2b390c738523eab5065b
GilbertoPaiva <89491606+GilbertoPaiva@users.noreply.github.com>
2023-12-19 00:15:27 +0000

    Percorrendo Array

---
+++ edu/gilberto/LivroJavaParaIniciantes/MinMax/src/MinMax.java
@@ -0,0 +1,14 @@
+public class MinMax {
+    public static void main(String[] args) throws Exception {
+        int nums[] = {99, -10, 100123, 18, -978, 5626, 463, -9, 287, 49};
+        int min, max;
+
+        min = max = nums[0];
+
+        for(int i = 1; i < 10; i++){
+            if(nums[i] < min) min = nums[i];
+            if(nums[i] > max) max = nums[i];
+        }
+        System.out.println("Min and Max: " + min + " " + max);
+    }
+}
