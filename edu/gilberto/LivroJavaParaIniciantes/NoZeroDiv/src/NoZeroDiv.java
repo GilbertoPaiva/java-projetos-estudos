6afd32666668cb5545ab6d973de327319f68f615
GilbertoPaiva <89491606+GilbertoPaiva@users.noreply.github.com>
2024-01-07 15:22:18 +0000

    Impede uma divisão por zero usando o operador ?

---
+++ edu/gilberto/LivroJavaParaIniciantes/NoZeroDiv/src/NoZeroDiv.java
@@ -0,0 +1,11 @@
+public class NoZeroDiv {
+    public static void main(String[] args) throws Exception {
+        int result;
+
+        for(int i = -5; i < 6; i++){
+            result = i != 0 ? 100/i : 0; // Essa parte impede uma divisão por zero.
+            if(i != 0)
+                System.out.println("100 / " + i + " is " + result);
+        }
+    }
+}
